fd97b5ea3e8f93b1ba5497c06a79c219738ba080
Adam Cardoso <adamjuniorcardoso@gmail.com>
2024-02-06 01:45:05 -0300

    test: adicionando timeout

--- selenium-java/src/test/java/utils/PageObject.java
+++ selenium-java/src/test/java/utils/PageObject.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class PageObject {
     protected WebDriver browser;
@@ -16,6 +17,8 @@ public class PageObject {
         }else {
             this.browser = browser;
         }
+
+        this.browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     public void fechar() {
